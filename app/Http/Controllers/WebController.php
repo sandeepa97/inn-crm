ad1ee1471061c9a68dd7ae232133237ccc6461ef
sandeepa97 <lbsandeepa@gmail.com>
2023-03-25 21:45:02 +0530

    web phase initiated

---
+++ app/Http/Controllers/WebController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WebController extends Controller
+{
+    public function index() {
+        return view('website.index');
+    }
+}
