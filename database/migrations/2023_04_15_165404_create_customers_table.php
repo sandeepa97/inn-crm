05532b9949541d47fa32434151c14ed379e1f037
sandeepa97 <lbsandeepa@gmail.com>
2023-04-16 20:15:32 +0530

    customer table don

--- database/migrations/2023_04_15_165404_create_customers_table.php
+++ database/migrations/2023_04_15_165404_create_customers_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Carbon\Carbon;
 
 return new class extends Migration
 {
@@ -13,6 +14,28 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('saluation')->nullable();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('mobile')->nullable();
+            $table->string('nic')->nullable();
+            $table->string('passport')->nullable();
+            $table->string('email')->nullable();
+            $table->string('country')->nullable();
+            $table->string('location')->nullable();
+            $table->string('username')->nullable();
+            $table->string('password')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->enum('disabled', ['t', 'f'])->default('f');
+            $table->dateTime('disabled_date')->nullable();
+            $table->string('disabled_reason')->nullable();
+            $table->string('disabled_by')->nullable();
+            $table->string('secret_question')->nullable();
+            $table->string('secret_answer')->nullable();
+            $table->dateTime('creation_date')->default(Carbon::now());
+            $table->string('created_by')->nullable();
+            $table->dateTime('update_date')->nullable();
+            $table->string('updated_by')->nullable();
             $table->timestamps();
         });
     }
