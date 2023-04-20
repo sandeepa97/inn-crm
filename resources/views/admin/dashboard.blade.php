e90ab471ba3326d3f048bc008a127a00c4d00d54
sandeepa97 <lbsandeepa@gmail.com>
2023-04-20 22:27:23 +0530

    dashboard ui fixes

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -32,7 +32,7 @@
           <div class="inner">
             <h3>150</h3>
 
-            <p>New Orders</p>
+            <p>Today Reservations</p>
           </div>
           <div class="icon">
             <i class="ion ion-bag"></i>
@@ -45,9 +45,9 @@
         <!-- small box -->
         <div class="small-box bg-success">
           <div class="inner">
-            <h3>53<sup style="font-size: 20px">%</sup></h3>
+            <h3>53<sup style="font-size: 20px"></sup></h3>
 
-            <p>Bounce Rate</p>
+            <p>Pending Reservations</p>
           </div>
           <div class="icon">
             <i class="ion ion-stats-bars"></i>
@@ -62,7 +62,7 @@
           <div class="inner">
             <h3>44</h3>
 
-            <p>User Registrations</p>
+            <p>Customers</p>
           </div>
           <div class="icon">
             <i class="ion ion-person-add"></i>
